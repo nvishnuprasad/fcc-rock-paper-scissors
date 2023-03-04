6352cb7c55055df398f098fac169f6cd09d640ec
Vishnu Prasad N <vishnuprasad.n@ni.com>
2023-03-04 18:26:51 +0530

    add main

--- app.js
+++ app.js
@@ -3,22 +3,25 @@ const computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computerScore-score");
 
-const scoreBoard_div = document.querySelector(".score-board")
-const result_div = document.querySelector(".result")
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
 
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
+main();
 
-rock_div.addEventListener('click', function() {
-    console.log("You clicked on rock")
-})
+function main() {
+  rock_div.addEventListener("click", function () {
+    console.log("You clicked on rock");
+  });
 
-paper_div.addEventListener('click', function() {
-    console.log("You clicked on paper")
-})
+  paper_div.addEventListener("click", function () {
+    console.log("You clicked on paper");
+  });
 
-scissors_div.addEventListener('click', function() {
-    console.log("You clicked on scissor")
-})
+  scissors_div.addEventListener("click", function () {
+    console.log("You clicked on scissor");
+  });
+}
