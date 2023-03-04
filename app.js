4b7f9be4450df2546aaf21623713b372fab834b6
Vishnu Prasad N <vishnuprasad.n@ni.com>
2023-03-04 18:15:14 +0530

    dom cache

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("hello")
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computerScore-score");
+
+const scoreBoard_div = document.querySelector(".score-board")
+const result_div = document.querySelector(".result")
+
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissor_div = document.getElementById("scissors");
+
+
